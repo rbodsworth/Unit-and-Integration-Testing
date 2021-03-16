cadfa363fc5fdc464e87164d9382244d36ff7462
Richard Bodsworth <richardbodsworth@gmail.com>
2021-03-16 14:44:14 +0000

    tests passing

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -8,5 +8,52 @@ describe('calculator', () => {
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
-  })
-})
+  });
+
+  it('math operations should update the display with the result of the operation', () => {
+    cy.get('#number6').click();
+    cy.get('#operator_add').click();
+    cy.get('#number6').click();
+    cy.get('#operator_add').click();
+    cy.get('.display').should('contain', '12')
+  });
+
+  it('should update the display with the result of the operation', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('.display').should('contain', '18')
+  }); 
+
+  it('should have a negative number as a total', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number10').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-1')
+  }); 
+
+  it(' should have a decimal number as a total', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number5').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.8')
+  });
+
+  it('should be a very large number', () => {
+    cy.get('#number6').click();
+    cy.get('#number6').click();
+    cy.get('#number6').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number6').click();
+    cy.get('#number6').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number6').click();
+    cy.get('#number6').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2901096')
+  }); 
+
+});
--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -10,3 +10,60 @@ describe('App.vue', () => {
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
 })
+
+it('subtracts two numbers and returns the result', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  });
+
+  it('multiplies two numbers and returns the result', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  });
+
+  it('divides two numbers and returns the result', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  });
+
+  it('concatenates multiple number button clicks', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 0;
+    wrapper.vm.runningTotal = 0;
+    wrapper.vm.newTotal = true;
+    wrapper.vm.numberClick('2');
+    wrapper.vm.numberClick('8');
+    expect(wrapper.vm.runningTotal).to.equal(28)
+  });
+
+  it('chains multiple operations together', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.runningTotal = 45;
+    wrapper.vm.previousTotal = 0;
+    wrapper.vm.newTotal = false;
+    wrapper.vm.operatorClick('*');
+    wrapper.vm.numberClick('4');
+    wrapper.vm.operatorClick('-');
+    wrapper.vm.numberClick('5');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(175)
+  });
+
+  it('clears the running total without affecting the calculation', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.runningTotal = 70;
+    wrapper.vm.previousTotal = 0;
+    wrapper.vm.newTotal = false;
+    wrapper.vm.operatorClick('-');
+    wrapper.vm.numberClick('6');
+    wrapper.vm.clearClick();
+    wrapper.vm.numberClick('3');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(67)
+  });
